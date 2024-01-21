269109595bc4957954128ce3eb0a9c88492eaa41
ChrisRobby <144351829+ChrisRobby@users.noreply.github.com>
2024-01-21 14:01:42 -0500

    Add the getWord and initGuess functions

--- RST/hangman.py
+++ RST/hangman.py
@@ -1,4 +1,5 @@
 # The main hangman program
+import random
 
 # The maximum number of lives 
 MAX_LIVES = 10
@@ -8,7 +9,31 @@ ERROR = -1
 LOSE  = 0
 WIN   = 1
 
+dictionary = [ "the dark knight", "iron man" , "captain america" , "avengers" , 
+               "the hulk" , "deadpool" , "spider man" , "darth vader" , "luke skywalker", 
+               "han solo" , "yoda" , "star wars" , "the terminator" , "green goblin" , 
+               "venom" , "obi-wan kenobi" , "stormtrooper" , "jedi" , "homer simpson" , 
+               "batman"];
 
+# get a random word from the dictionary 
+def getWordFromDictionary():
+    length = len(dictionary)
+    index = random.randrange(0,length)
+    return dictionary[index]
+
+# initialize the guess array from the phrase
+# leters must be '_' and spaces left as a spaces
+def initGuessStr(phrase):
+    guessStr = ""
+    for c in phrase:
+        if c >= 'a' and c <= 'z':
+            guessStr = guessStr + "_"
+        else:
+            guessStr = guessStr + c
+    
+    return guessStr
+
+# start the game of hangman with maxLives
 def startgame(maxLives):
     lives = int(maxLives)
     
@@ -16,7 +41,18 @@ def startgame(maxLives):
     if (lives < 1 or lives > MAX_LIVES ):
         print( "Error: maxLives not valid = %d\n" % lives )
         return ERROR
+
+    result = LOSE
+    
+    # get a random word
+    phrase = getWordFromDictionary()
+    print(phrase)
+    
+    # initialze the guess array based on the phrase
+    guessStr = initGuessStr(phrase)
+    print(guessStr)
+    
     
     ## return result
-    return WIN
+    return result
 
